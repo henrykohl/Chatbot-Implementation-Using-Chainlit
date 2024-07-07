111efb0b0aa1e7ad6c23155c1b9f77fffcf3823f
Henry Hsu <43148403+henrykohl@users.noreply.github.com>
2024-07-07 08:40:53 -0500

    files created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Henry Kohl",
+    author_email= "henglikohl425@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
